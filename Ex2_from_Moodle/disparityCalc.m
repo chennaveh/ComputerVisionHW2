f07f8df39cd425155fdb45170fd4a59acd4cf97c
Chen Naveh <navehchen1@gmail.com>
2018-11-30 15:49:45 +0200

    Fix disparityCalc implementation

--- Ex2_from_Moodle/disparityCalc.m
+++ Ex2_from_Moodle/disparityCalc.m
@@ -8,34 +8,32 @@ height = size(im1,1);
 new_height = height+Sy-mod(Sy,2);
 new_width = width+Sx-mod(Sx,2);
 
-im1_padded = zeros(new_height,new_width); % pad zero frame
-im1_padded (Sy/2:end-Sy/2, Sx/2:end-Sx/2) = im1;
-
-im2_padded = zeros(new_height,new_width); % pad zero frame
-im2_padded(Sy/2:end-Sy/2, Sx/2:end-Sx/2) = im2;
-%mem = zeros(size(im2_padded))-1;
-D = zeros(size(im1));
-
 %loop over epipolar lines and for each pair calc the disparity
 Sy_half_flr = floor(Sy/2);
 Sx_half_flr = floor(Sx/2);
 
-for i=ceil(Sx/2):new_width-ceil(Sx/2)-d_min
-    for j=ceil(Sy/2):new_height-ceil(Sy/2)+1
-        %need to add minimum on the range dmin:dmax
-        v1 = reshape (im1_padded(j-Sy_half_flr:j+Sy_half_flr,i-Sx_half_flr :i+Sx_half_flr),[1,Sx*Sy]);
-        x=1;
-        for d=i+d_min:i+d_max
-            if d < new_width-ceil(Sx/2)+1
-                 %   if mem(i,d)==-1 %memoization for each pair option i (first pic) and d (second pic)
-                v2 = reshape (im2_padded(j-Sy_half_flr:j+Sy_half_flr,d-Sx_half_flr :d+Sx_half_flr ),[1,Sx*Sy]);
-                distance(x) = (v1*v2')/(sqrt(sum(v1.^2))*sqrt(sum(v2.^2)));
-                x=x+1;
-            end
+im1_padded = zeros(new_height,new_width); % pad zero frame
+im1_padded (Sy_half_flr+1:end-Sy_half_flr, Sx_half_flr+1:end-Sx_half_flr) = im1;
+
+im2_padded = zeros(new_height,new_width); % pad zero frame
+im2_padded(Sy_half_flr+1:end-Sy_half_flr, Sx_half_flr+1:end-Sx_half_flr) = im2;
+
+D = zeros(size(im1));
+
+
+for i = 1:height 
+    for j = 1:width
+        patch1 = im1_padded(i:i+Sy-1, j:j+Sx-1);
+        start_idx = min(j+d_min,width);
+        end_idx   = min(j+d_max,width);
+        results_vec = zeros(1, end_idx - start_idx + 1);
+        for k = start_idx:end_idx
+            patch2 = im2_padded(i:i+Sy-1, k:k+Sx-1);
+            results_vec(k - start_idx + 1) = sum(patch1.*patch2,'all') / sqrt((sum(patch1.^2, 'all')*sum(patch2.^2, 'all')));
         end
-        [~, index] = min(distance);
-        D(j-Sy_half_flr,i-Sx_half_flr) = index;
+        [~, index] = min(results_vec);
+        D(i,j) = index;
     end
 end
-end
 
+end
